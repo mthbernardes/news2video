93451c82017b00ab717be1d888e7cbf3ce4f96e7
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-03-08 22:27:21 -0300

    Update video.clj

--- src/news2video/diplomat/video.clj
+++ src/news2video/diplomat/video.clj
@@ -22,9 +22,9 @@
     filename))
 
 (defn merge! [videos]
-  (let [output-video "/tmp/output-video.mkv"
+  (let [output-video "/tmp/output-video.mp4"
         videos-to-merge (generate-videos-to-merge videos)
-        command ["ffmpeg" "-f" "concat" "-safe" "0" "-i" videos-to-merge "-vcodec" "libx264" "-crf" "28" "-preset" "faster" "-tune" "film" "-c" "copy" output-video]
+        command ["ffmpeg" "-f" "concat" "-safe" "0" "-i" videos-to-merge "-vcodec" "libx264" "-crf" "0" "-preset" "faster" "-shortest" "-c" "copy" output-video]
         execution (apply shell/sh command)]
     (if (-> execution :exit zero?)
       output-video
